57a35fb4aac39566e5da70e5d272acedb1d1b51e
Damian <kuciel.damian2@gmail.com>
2022-11-10 23:24:16 +0100

    GUI

---
+++ src/main/java/DMS_GUI.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class DMS_GUI {
+
+
+
+
+    private JButton Button;
+    private JTextField txtT1;
+    private JTextField txtT2;
+    private JTextField txtT3;
+    private JTextField txtT4;
+    private JTextField txtT5;
+    private JTextField txtP1;
+    private JTextField txtP2;
+    private JTextField txtP3;
+    private JTextField txtP4;
+    private JTextField txtP5;
+    private JTextField txtD1;
+    private JTextField txtD2;
+    private JTextField txtD3;
+    private JTextField txtD4;
+    private JTextField txtD5;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
